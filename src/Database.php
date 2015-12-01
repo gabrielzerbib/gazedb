1d319a988cda3db782a383a2de41bd75d38ac034
Gabriel <gabriel.zerbib@gmail.com>
2015-12-01 14:35:50 +0200

    Default fetch mode assoc

--- src/Database.php
+++ src/Database.php
@@ -73,6 +73,7 @@ class Database
     if (null == $this->pdo) {
       $this->pdo = new \PDO($this->dsn, $this->username, $this->password);
       $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+      $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
     }
     return $this->pdo;
   }
