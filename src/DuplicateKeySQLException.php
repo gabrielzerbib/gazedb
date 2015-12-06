1aa55ebbf06f03efc2020b5284326870b45dd118
Gabriel Zerbib <gabriel.zerbib@gmail.com>
2015-12-06 17:02:08 +0200

    missing const

--- src/DuplicateKeySQLException.php
+++ src/DuplicateKeySQLException.php
@@ -4,6 +4,6 @@ namespace gazedb;
 
 class DuplicateKeySQLException extends SQLException {
   public function __construct($query, $errMsg, \Exception $previous = null) {
-    parent::__construct($query, $errMsg, Database::ERROR_DUPLICATE_KEY, $previous);
+    parent::__construct($query, $errMsg, 0, $previous);
   }
 }
